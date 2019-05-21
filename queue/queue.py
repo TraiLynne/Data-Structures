599dc89a058887d7b2f3725e571cfff6ef4aaf93
Trai Lynne <trailynne@gmail.com>
2019-05-21 14:02:21 -0600

    Solved `queue` Changed from LinkedList to regular list

--- queue/queue.py
+++ queue/queue.py
@@ -1,73 +1,16 @@
-class Node:
-    def __init__(self, value=None, next_node=None):
-        self.value = value
-        self.next_node = next_node
-
-    def get_value(self):
-        return self.value
-
-    def get_next(self):
-        return self.next_node
-
-    def set_next(self, new_next):
-        self.next_node = new_next
-
-    def __repr__(self):
-        return '{}'.format(self.value)
-
-
-class LinkedList:
-    def __init__(self):
-        self.head = None
-        self.tail = None
-
-    def add_to_tail(self, value):
-        new_node = Node(value, None)
-        if not self.head:
-            self.head = new_node
-            self.tail = new_node
-        else:
-            self.tail.set_next(new_node)
-            self.tail = new_node
-
-    def remove_head(self):
-        value = self.head.get_value()
-        if not self.head.get_next():
-            self.head = None
-            self.tail = None
-            return value
-
-        self.head = self.head.get_next()
-
-        return value
-
-    def contains(self, value):
-
-        if not self.head:
-            return False
-
-        if self.head.get_value() == value or self.tail.get_value() == value:
-            return True
-        
-        current = self.head
-        
-        while current:
-            if current.get_value() == value:
-                return True
-            current = current.get_next()
-        return False
-
-
 class Queue:
   def __init__(self):
     self.size = 0
-    self.storage = LinkedList()
+    self.storage = []
 
   def enqueue(self, item):
-    self.storage.add_to_tail(item)
+    self.storage.append(item)
+    self.size += 1
   
   def dequeue(self):
-    self.storage.remove_head()
+    if self.size > 0:
+      self.size -= 1
+      return self.storage.pop(0)
 
   def len(self):
-    len(self.storage)
+    return self.size
